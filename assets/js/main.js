8b459cc694ec618004d5f7269d4d3417daa5bf22
esrojas27 <62344298+esrojas27@users.noreply.github.com>
2020-08-25 19:34:44 -0500

    Primer Commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+var nombre = document.getElementById('nombre')
+var edad = document.getElementById('edad')
+
+function cantante(){
+
+	var name = prompt("Nombre dell personaje")
+	nombre.textContent = name
+
+	var age = prompt("Edad del personaje")
+	edad.innerText = age
+
+}
